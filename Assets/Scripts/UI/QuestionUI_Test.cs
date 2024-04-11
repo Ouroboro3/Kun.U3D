35c9f3c5ded161fbe77446140fd19dca2fa719c8
Ouroboro3 <2528378776@qq.com>
2024-04-12 00:07:25 +0800

    答题界面完成，后续换入主场景更改文本内容即可，UI贴图均替换

--- Assets/Scripts/UI/QuestionUI_Test.cs
+++ Assets/Scripts/UI/QuestionUI_Test.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class QuestionUI_Test : MonoBehaviour
 {
     public bool result = false;
+    public TalkUI talk;
+    private void Start()
+    {
+        talk = GetComponent<TalkUI>();
+    }
+
+    public void Right()
+    {
+        result = true;
+        talk.QuestionBox.SetActive(false);
+    }
 }
